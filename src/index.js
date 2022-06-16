c3f85396b9724fe645ddb79bf223c896db2c2dfb
Samuel Abioye <101730798+AbioyeSamuel@users.noreply.github.com>
2022-06-16 18:26:02 +0100

    Render html elements in react import React and ReactDOM instaed of requiring the modules and rendering html elements in react index.js

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Hello world!</h1>
+    <p>This is Samuel</p>
+  </div>,
+  document.getElementById("root")
+);
+
+//how it would be in vanilla javascript.
+// var h1 = document.createElement('h1');
+// h1.innerHTML = "Hello World!";
+// document.getElementById("root").appendChild(h1);
