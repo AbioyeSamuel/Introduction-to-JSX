fd7a9763e2c0b10a75dc740d015b5ca6ddafdc7d
Samuel Abioye <101730798+AbioyeSamuel@users.noreply.github.com>
2022-06-17 11:02:56 +0100

    Inline styling for React (JSX) Elements. Inline styling for React JSX Elements.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const customStyle = {
+  color: "red",
+  fontSize: "20px",
+  border: "1px solid balck"
+};
 ReactDOM.render(
   <div>
-    <h1>Hello world!</h1>
+    <h1 style={customStyle}>Hello world!</h1>
+    <h6 style={{ color: "red" }}>Hi!</h6>
     <p>This is Samuel</p>
   </div>,
   document.getElementById("root")
